d7ca8743b6b954b5b3ffd88f38a288d8ecfcfcc6
sizar <sizar@gmail.com>
2023-03-19 11:30:51 +0600

    update

--- asset_approve.php
+++ asset_approve.php
@@ -1,7 +1,7 @@
 <html>
     <head>
      <meta charset="UTF-8" />
-     <meta http-equiv="refresh" content="0; URL=http://192.168.0.100/outlet/oms/dashboard.php#asset-table" />
+     <meta http-equiv="refresh" content="0; URL=http://localhost/outlet/oms/dashboard.php#asset-table" />
    </head>
 </html>
 <?php
